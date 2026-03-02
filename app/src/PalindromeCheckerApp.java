e4d013c17d3ca90ff650a51d526c7a61e2a60391
Manya Sharma <manya.s1008@gmail.com>
2026-03-02 14:41:29 +0530

    UC1 is done

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
 public class PalindromeCheckerApp {
+public static void main(String[] args){
+    System.out.println("Welcome to Palindrome Checker App Management System");
+   }
 }
